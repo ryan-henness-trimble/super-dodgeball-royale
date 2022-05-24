91373452ecd552bd72efd737ed9c9693be991b95
Ryan Henness <ryan_henness@trimble.com>
2022-05-24 12:27:49 -0700

    Revert SERVER_URL

--- src/server/testserver.js
+++ src/server/testserver.js
@@ -1,6 +1,6 @@
 const { GameServer } = require('./GameServer');
 
 const cors = ['http://localhost:8080', 'https://super-dodgeball-royale.herokuapp.com'];
-const s = new GameServer(cors, 'https://super-dodgeball-royale-server.herokuapp.com/');
+const s = new GameServer(cors, process.env.PORT | 8090);
 
 s.startListening();
